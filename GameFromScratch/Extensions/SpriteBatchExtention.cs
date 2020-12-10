348a5bc91d5455d5762fbc3c18573897e549748a
Daniel <danlpsarcanjo@gmail.com>
2020-12-10 17:53:57 -0500

    Create DrawRichString. Improve RichString

--- GameFromScratch/Extensions/SpriteBatchExtention.cs
+++ GameFromScratch/Extensions/SpriteBatchExtention.cs
@@ -1,26 +1,63 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using GameFromScratch.Utils;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace GameFromScratch.Extensions
 {
     public static class SpriteBatchExtension
     {
-        public static void DrawFillRectangle(this SpriteBatch spriteBatch, GraphicsDevice graphicsDevice, Rectangle rectangle, Color color)
+        public static void DrawFillRectangle(this SpriteBatch spriteBatch, GraphicsDevice graphicsDevice,
+            Rectangle rectangle, Color color)
         {
             var tex = new Texture2D(graphicsDevice, 1, 1);
-            tex.SetData(new[] { Color.White });
+            tex.SetData(new[] {Color.White});
 
             spriteBatch.Draw(tex, rectangle, color);
         }
-        public static void DrawRectangle(this SpriteBatch spriteBatch, GraphicsDevice graphicsDevice, Rectangle rectangle, Color color, int borderWidth = 1)
+
+        public static void DrawRectangle(this SpriteBatch spriteBatch, GraphicsDevice graphicsDevice,
+            Rectangle rectangle, Color color, int borderWidth = 1)
         {
             var tex = new Texture2D(graphicsDevice, 1, 1);
-            tex.SetData(new[] { Color.White });
+            tex.SetData(new[] {Color.White});
 
             spriteBatch.Draw(tex, new Rectangle(rectangle.X, rectangle.Y, rectangle.Width, borderWidth), color);
-            spriteBatch.Draw(tex, new Rectangle(rectangle.X + rectangle.Width - borderWidth, rectangle.Y, borderWidth, rectangle.Height), color);
-            spriteBatch.Draw(tex, new Rectangle(rectangle.X, rectangle.Y + rectangle.Height - borderWidth, rectangle.Width, borderWidth), color);
+            spriteBatch.Draw(tex,
+                new Rectangle(rectangle.X + rectangle.Width - borderWidth, rectangle.Y, borderWidth, rectangle.Height),
+                color);
+            spriteBatch.Draw(tex,
+                new Rectangle(rectangle.X, rectangle.Y + rectangle.Height - borderWidth, rectangle.Width, borderWidth),
+                color);
             spriteBatch.Draw(tex, new Rectangle(rectangle.X, rectangle.Y, borderWidth, rectangle.Height), color);
+        }
+
+        public static void DrawRichString(this SpriteBatch spriteBatch, SpriteFont font, RichString richString,
+            Vector2 position)
+        {
+            var (x, y) = position;
+            for (int i = 0; i < richString.Count; i++)
+            {
+                var width = font.MeasureString(richString.Strings[i]).X;
+                var color = richString.Colors[i];
+                
+                var texts = richString.Strings[i]
+                    .Split(new[] {Environment.NewLine, "\n", "\r\n"}, StringSplitOptions.None);
+                for (var j = 0; j < texts.Length; j++)
+                {
+                    var text = texts[j];
+                    spriteBatch.DrawString(font, text, new Vector2(x, y), color);
+                    if (texts.Length > 1 && j < texts.Length - 1)
+                    {
+                        y += font.LineSpacing;
+                        x = position.X;
+                    }
+                    else
+                    {
+                        x += font.MeasureString(text).X;
+                    }
+                }
+            }
         }
     }
 }
--- GameFromScratch/Utils/RichString.cs
+++ GameFromScratch/Utils/RichString.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Microsoft.Xna.Framework;
 
 namespace GameFromScratch.Utils
@@ -7,11 +9,28 @@ namespace GameFromScratch.Utils
     {
         private List<string> strings = new List<string>();
         private List<Color> colors = new List<Color>();
-        
-        public List<string> Strings { get => new List<string>(strings); }
-        public List<Color> Colors { get => new List<Color>(colors); }
+
+        public List<string> Strings
+        {
+            get => new List<string>(strings);
+        }
+
+        public List<Color> Colors
+        {
+            get => new List<Color>(colors);
+        }
+
         public int Count { get; private set; }
-        
+        public Color DefaultColor { get; set; }
+        public RichString(Color defaultColor)
+        {
+            DefaultColor = defaultColor;
+        }
+
+        public RichString() : this(Color.White)
+        {
+        }
+
         public RichString Append(string text, Color color)
         {
             strings.Add(text);
@@ -19,6 +38,39 @@ namespace GameFromScratch.Utils
             Count++;
             return this;
         }
+        public RichString Append(string text)
+        {
+            return Append(text, DefaultColor);
+        }
+        
+        // public RichString AppendLine(string text, Color color)
+        // {
+        //     strings.Add(text + Environment.NewLine);
+        //     colors.Add(color);
+        //     Count++;
+        //     return this;
+        // }
+        // public RichString AppendLine(string text)
+        // {
+        //     return Append(text, DefaultColor);
+        // }
 
+        public void Clear()
+        {
+            strings.Clear();
+            colors.Clear();
+            Count = 0;
+        }
+        
+        public override string ToString()
+        {
+            var text = new StringBuilder();
+            foreach (var s in strings)
+            {
+                text.Append(s);
+            }
+
+            return text.ToString();
+        }
     }
 }
