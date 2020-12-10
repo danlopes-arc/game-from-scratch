6c1ad77313e256beb74a50a5cbec20f242c5f2d5
Daniel <danlpsarcanjo@gmail.com>
2020-12-10 15:24:18 -0500

    Create RichString class

---
+++ GameFromScratch/Utils/RichString.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace GameFromScratch.Utils
+{
+    public class RichString
+    {
+        private List<string> strings = new List<string>();
+        private List<Color> colors = new List<Color>();
+        
+        public List<string> Strings { get => new List<string>(strings); }
+        public List<Color> Colors { get => new List<Color>(colors); }
+        public int Count { get; private set; }
+        
+        public RichString Append(string text, Color color)
+        {
+            strings.Add(text);
+            colors.Add(color);
+            Count++;
+            return this;
+        }
+
+    }
+}
