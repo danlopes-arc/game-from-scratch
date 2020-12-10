658ac3e1d1c80931cd9569c2403d53af6a34b48c
Daniel <danlpsarcanjo@gmail.com>
2020-12-10 00:54:05 -0500

    Create MainMenu and add it to StartScene
    The menu works partially. It responds to user input but it doesn't
    select the respective Scene

---
+++ GameFromScratch/Components/MainMenu.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GameFromScratch.Utils;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameFromScratch.Components
+{
+    public class MainMenu : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        private SpriteFont font;
+        private List<string> items;
+        private List<Rectangle> boundList;
+        private Vector2 position;
+        private int width = -1;
+
+
+        public int Displacement { get; set; }
+        public int Index { get; set; }
+
+        public int Width
+        {
+            get
+            {
+                if (width >= 0) return width;
+
+                foreach (var item in items)
+                {
+                    var x = font.MeasureString(item).X;
+                    if (x > width) width = (int) x;
+                }
+
+                return width;
+            }
+        }
+
+        public MainMenu(SpriteBatch spriteBatch, Game game, List<string> items, Vector2 position) : base(game)
+        {
+            this.items = items;
+            this.position = position;
+            this.spriteBatch = spriteBatch;
+            font = Game.Content.Load<SpriteFont>("Fonts/ScreenInfo");
+        }
+
+        private void CreateList()
+        {
+            var heignt = font.LineSpacing;
+            var width = 0;
+            foreach (var item in items)
+            {
+                var x = font.MeasureString(item).X;
+                if (x > width) width = (int) x;
+            }
+
+            boundList = new List<Rectangle>();
+            for (int i = 0; i < items.Count; i++)
+            {
+                boundList.Add(new Rectangle(position.ToPoint(), new Point(width + Displacement, heignt * i)));
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (BetterKeyboardState.IsJustDown(Keys.Down) || BetterKeyboardState.IsJustDown(Keys.S))
+            {
+                if (++Index > items.Count - 1) Index = 0;
+            }
+
+            if (BetterKeyboardState.IsJustDown(Keys.Up) || BetterKeyboardState.IsJustDown(Keys.W))
+            {
+                if (--Index < 0) Index = items.Count - 1;
+            }
+
+            if (BetterMouseState.Displacement != Point.Zero)
+            {
+                if (BetterMouseState.Current.X >= position.X && BetterMouseState.Current.X < position.X + Width)
+                {
+                    var i = (int) ((BetterMouseState.Current.Y - position.Y) / font.LineSpacing);
+                    if (i >= 0 && i < items.Count)
+                        Index = i;
+                }
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            spriteBatch.Begin();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                var color = Color.White;
+                if (i == Index)
+                {
+                    color = Color.DarkBlue;
+                    spriteBatch.DrawString(font, items[i], position + new Vector2(Displacement, font.LineSpacing * i + Displacement), Color.Black * .33f);
+                }
+                spriteBatch.DrawString(font, items[i], position + new Vector2(0, font.LineSpacing * i), color);
+            }
+
+            spriteBatch.End();
+        }
+    }
+}
--- GameFromScratch/Scenes/StartScene.cs
+++ GameFromScratch/Scenes/StartScene.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using GameFromScratch.Components;
 using GameFromScratch.Utils;
 
 namespace GameFromScratch.Scenes
@@ -11,9 +12,19 @@ namespace GameFromScratch.Scenes
     public class StartScene : GameScene
     {
         private SpriteFont font;
+        private MainMenu menu;
+
         public StartScene(GameMain game, SpriteBatch spriteBatch) : base(game, spriteBatch)
         {
             font = Game.Content.Load<SpriteFont>("Fonts/ScreenInfo");
+            menu = new MainMenu(spriteBatch,
+                game,
+                new List<string>() {"Play", "Help", "About", "Quit"},
+                new Vector2(100, 100))
+            {
+                Displacement = 2
+            };
+            components.Add(menu);
         }
 
         public override void Update(GameTime gameTime)
@@ -31,11 +42,12 @@ namespace GameFromScratch.Scenes
             base.Draw(gameTime);
 
             spriteBatch.Begin();
-            var text = $"START GAME{Environment.NewLine}" +
-                $"Press ENTER to Start";
-            var x = Game.GraphicsDevice.Viewport.Width / 2f - font.MeasureString(text).X / 2;
-            var y = Game.GraphicsDevice.Viewport.Height / 2f - font.MeasureString(text).Y / 2;
-            spriteBatch.DrawString(font, text, new Vector2(x, y), Color.White);
+            var text = $"Press Up/W or Down/S to navigate{Environment.NewLine}" +
+                       $"Press ENTER to select";
+            // var x = Game.GraphicsDevice.Viewport.Width / 2f - font.MeasureString(text).X / 2;
+            // var y = Game.GraphicsDevice.Viewport.Height / 2f - font.MeasureString(text).Y / 2;
+            spriteBatch.DrawString(font, text,
+                new Vector2(100, Game.GraphicsDevice.Viewport.Height - font.MeasureString(text).Y - 20), Color.White);
 
             spriteBatch.End();
         }
