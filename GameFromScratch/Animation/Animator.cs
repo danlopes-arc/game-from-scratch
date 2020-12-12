ca9b143e7b26ae55c049838aadbf66a94937b13f
Daniel <danlpsarcanjo@gmail.com>
2020-12-12 13:14:24 -0500

    Create Sequence and Animator classes

---
+++ GameFromScratch/Animation/Animator.cs
@@ -0,0 +1,95 @@
+﻿using GameFromScratch.Utils;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameFromScratch.Animation
+{
+    public class Animator
+    {
+        private SpriteBatch spriteBatch;
+        private Counter counter;
+        private Sequence sequence;
+        public Sequence Sequence
+        {
+            get => sequence;
+            set
+            {
+                sequence = value;
+                counter = new Counter(sequence.FrameDuration);
+            }
+        }
+
+        public bool Loop { get; set; }
+        public int LoopLimit { get; set; }
+        public int LoopCount { get; private set; }
+        public bool Complete { get; private set; }
+        public bool Playing { get; private set; }
+        public int Index { get; private set; }
+
+        public Animator(SpriteBatch spriteBatch)
+        {
+            this.spriteBatch = spriteBatch;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (!Playing || Complete || Sequence == null) return;
+            
+            if (counter.Update(deltaTime))
+            {
+                if (++Index == Sequence.Count)
+                {
+                    if (!Loop || ++LoopCount == LoopLimit)
+                    {
+                        Complete = true;
+                        Playing = false;
+                    }
+                    else
+                    {
+                        Index = 0;
+                    }
+                }
+                counter.Reset();
+            }
+            
+        }
+
+        public void Draw(Point position, Point size)
+        {
+            if (!Playing || Complete || Sequence == null) return;
+            var destRect = new Rectangle(position, size);
+            
+            spriteBatch.Draw(Sequence.Texture, destRect, Sequence[Index], Color.White);
+        }
+
+        public bool Play()
+        {
+            if (Playing)
+            {
+                return false;
+            }
+
+            Playing = true;
+            return true;
+        }
+        
+        public bool Pause()
+        {
+            if (!Playing || Complete)
+            {
+                return false;
+            }
+
+            Playing = false;
+            return true;
+        }
+
+        public void Reset()
+        {
+            Playing = false;
+            Complete = false;
+            Index = 0;
+            counter.Reset();
+        }
+    }
+}
---
+++ GameFromScratch/Animation/Sequence.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameFromScratch.Animation
+{
+    public class Sequence
+    {
+        private Texture2D texture;
+        private Point size;
+        private int startIndex;
+        private int count;
+        private int rowCount;
+        private int colCount;
+        private Rectangle[] frames;
+        private float totalDuration;
+
+        public Sequence(Texture2D texture, Point size, float totalDuration, int startIndex, int count)
+        {
+            this.texture = texture;
+            this.size = size;
+            this.startIndex = startIndex;
+            this.totalDuration = totalDuration;
+            this.count = Math.Max(count, rowCount * colCount);
+
+            rowCount = texture.Width / size.X;
+            colCount = texture.Height / size.Y;
+
+            frames = new Rectangle[count];
+            for (int i = 0; i < Count; i++)
+            {
+                frames[i] = new Rectangle(new Point(i % ColCount * Size.X, i / RowCount * Size.Y), size);
+            }
+        }
+
+        public Sequence(Texture2D texture, Point size, float totalDuration, int startIndex = 0)
+            : this(texture, size, totalDuration, startIndex, texture.Width / size.X * texture.Height / size.Y)
+        {
+        }
+
+        public Rectangle this[int index]
+        {
+            get => frames[index];
+        }
+        public int Count => count;
+        public Texture2D Texture => texture;
+        public Point Size => size;
+        public int StartIndex => startIndex;
+        public int RowCount => rowCount;
+        public int ColCount => colCount;
+
+        public float TotalDuration => totalDuration;
+        public float FrameDuration
+        {
+            get => totalDuration / Count;
+        }
+    }
+}
