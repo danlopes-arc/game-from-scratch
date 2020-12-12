06792db43693d21a2446cb350cb7d906fbdf01a2
Daniel <danlpsarcanjo@gmail.com>
2020-12-12 13:56:40 -0500

    Make Missile create Explosion after destruction

--- GameFromScratch/Entities/Missile.cs
+++ GameFromScratch/Entities/Missile.cs
@@ -13,22 +13,22 @@ namespace GameFromScratch.Entities
             Velocity = new Vector2(300, 0);
             Texture = Game.Content.Load<Texture2D>("Images/Missile");
         }
-        
+
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-            
+
             Move(gameTime);
 
             if (Position.X > ScreenSize.X)
             {
                 Destroy();
             }
+
             // else if (Position.Y + Size.Y > ScreenSize.Y)
             // {
             //     Position = new Vector2(Position.X, ScreenSize.Y - Size.Y);
             // }
-
         }
 
         public override void OnCollision(Entity other)
@@ -37,6 +37,11 @@ namespace GameFromScratch.Entities
             {
                 other.Kill();
                 Destroy();
+                scene.AddEntity(new Explosion(scene, spriteBatch)
+                {
+                    Position = Position - new Vector2(0, Size.Y),
+                    Size = new Vector2(60, 60)
+                });
             }
         }
 
