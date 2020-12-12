9eb7e48d5504e2af1143f8c0fd515b50b00caff3
Daniel <danlpsarcanjo@gmail.com>
2020-12-12 16:16:36 -0500

    Create AsteroidExplosion and Explosion2

---
+++ GameFromScratch/Entities/Animations/AsteroidExplosion.cs
@@ -0,0 +1,40 @@
+﻿using GameFromScratch.Animation;
+using GameFromScratch.Scenes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameFromScratch.Entities.Animations
+{
+    public class AsteroidExplosion : Entity
+    {
+        public AsteroidExplosion(GameScene scene, SpriteBatch spriteBatch) : base(scene, spriteBatch)
+        {
+            animator.Sequence =
+                new Sequence(Game.Content.Load<Texture2D>("Sequences/RockExplosion"), new Point(274), .2f, 2, 5);
+            animator.Play();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (animator.Complete)
+            {
+                Destroy();
+                return;
+            }
+
+            animator.Update((float) gameTime.ElapsedGameTime.TotalSeconds);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            if (Destroyed) return;
+
+            spriteBatch.Begin();
+            animator.Draw(Position.ToPoint(), Size.ToPoint());
+            spriteBatch.End();
+        }
+    }
+}
---
+++ GameFromScratch/Entities/Animations/Explosion2.cs
@@ -0,0 +1,40 @@
+﻿using GameFromScratch.Animation;
+using GameFromScratch.Scenes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameFromScratch.Entities.Animations
+{
+    public class Explosion2 : Entity
+    {
+        public Explosion2(GameScene scene, SpriteBatch spriteBatch) : base(scene, spriteBatch)
+        {
+            animator.Sequence =
+                new Sequence(Game.Content.Load<Texture2D>("Sequences/Explosion2"), new Point(192), .2f);
+            animator.Play();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            if (animator.Complete)
+            {
+                Destroy();
+                return;
+            }
+
+            animator.Update((float) gameTime.ElapsedGameTime.TotalSeconds);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            if (Destroyed) return;
+
+            spriteBatch.Begin();
+            animator.Draw(Position.ToPoint(), Size.ToPoint());
+            spriteBatch.End();
+        }
+    }
+}
