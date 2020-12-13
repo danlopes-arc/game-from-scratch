6854c9187539be0c48ae45cd0fa1f27d5d74a84d
Daniel <danlpsarcanjo@gmail.com>
2020-12-13 00:04:46 -0500

    Create EnemyShip and ShipSpawner

---
+++ GameFromScratch/Entities/EnemyShip.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using GameFromScratch.Entities.Animations;
+using GameFromScratch.Extensions;
+using GameFromScratch.Scenes;
+using GameFromScratch.Utils;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameFromScratch.Entities
+{
+    public class EnemyShip : Entity
+    {
+        private const float MoveSpeed = 100;
+        private const float MoveDistance = 100;
+        
+        private SoundEffect explosionSound;
+        private Explosion2 explosionAnimation;
+
+        private Counter waitTimer = new Counter(2);
+        public bool HasShot { get; private set; }
+        public bool Waiting { get; private set; }
+
+        public EnemyShip(GameScene scene, SpriteBatch spriteBatch) : base(scene, spriteBatch)
+        {
+            Size = new Vector2(80, 80);
+            // Texture = Game.Content.Load<Texture2D>("Images/SpaceShipTopView");
+            // explosionSound = Game.Content.Load<SoundEffect>("SoundEffects/ShipExplosion");
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if (!Waiting)
+            {
+                Velocity = !HasShot ? new Vector2(-MoveSpeed, 0) : new Vector2(MoveSpeed, 0);
+            }
+            else
+            {
+                Velocity = Vector2.Zero;
+            }
+            Move(gameTime);
+
+            if (!HasShot && Position.X < ScreenSize.X - MoveDistance)
+            {
+                Waiting = true;
+            }
+            
+            if (HasShot && Position.X > ScreenSize.X)
+            {
+                Destroy();
+            }
+
+            if (Waiting && waitTimer.Update((float) gameTime.ElapsedGameTime.TotalSeconds))
+            {
+                if (!HasShot)
+                {
+                    // TODO: shoot
+                    HasShot = true;
+                    waitTimer.Reset();
+                }
+                else
+                {
+                    Waiting = false;
+                }
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            spriteBatch.Begin();
+
+            // spriteBatch.Draw(Texture, Bounds, Color.White);
+            spriteBatch.DrawRectangle(GraphicsDevice, Bounds, Color.Red);
+
+            spriteBatch.End();
+        }
+    }
+}
---
+++ GameFromScratch/Utils/ShipSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using GameFromScratch.Entities;
+using GameFromScratch.Scenes.Stages;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameFromScratch.Utils
+{
+    public class ShipSpawner : GameComponent
+    {
+        private EnemyShip ship;
+        private Counter shipTimer;
+        private SpriteBatch spriteBatch;
+        private Stage stage;
+        private Random r = new Random();
+        
+        public ShipSpawner(Game game, Stage stage, SpriteBatch spriteBatch) : base(game)
+        {
+            this.stage = stage;
+            this.spriteBatch = spriteBatch;
+            shipTimer = new Counter(3);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            var seconds = (float) gameTime.ElapsedGameTime.TotalSeconds;
+            base.Update(gameTime);
+
+            if (ship != null)
+            {
+                if (ship.Destroyed)
+                {
+                    ship = null;
+                    shipTimer.Reset();
+                }
+                return;
+            }
+            
+            if (!shipTimer.Update(seconds))
+            {
+                return;
+            }
+
+            ship = new EnemyShip(stage, spriteBatch);
+            var height = r.Next(stage.Bounds.Top ,stage.Bounds.Bottom - (int) ship.Size.Y);
+            ship.Position = new Vector2(stage.Bounds.Width, height);
+            stage.AddEntity(ship);
+        }
+    }
+}
